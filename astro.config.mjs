4c64a18b013a07a131b04376345f054538f7dd06
studentskeblokade <studentskeblokade@gmai.com>
2025-03-10 03:59:07 +0100

    optimize fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,24 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import icon from "astro-icon";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blokade.org',
-	integrations: [sitemap(), icon()],
-	redirects: {
-		'/sub': '/sr-lat/linkovi/studenti_u_blokadi',
-	},
-	i18n: {
-		locales: ["sr", "sr-lat", "en"],
-		defaultLocale: "sr",
-		fallback: {
-			en: "sr-lat"
-		},
-		routing: {
-			prefixDefaultLocale: false,
-			fallbackType: "rewrite"
-		}
-	}
+    site: 'https://blokade.org',
+    integrations: [sitemap(), icon(), purgecss()],
+    redirects: {
+        '/sub': '/sr-lat/linkovi/studenti_u_blokadi',
+    },
+    i18n: {
+        locales: ["sr", "sr-lat", "en"],
+        defaultLocale: "sr",
+        fallback: {
+            en: "sr-lat"
+        },
+        routing: {
+            prefixDefaultLocale: false,
+            fallbackType: "rewrite"
+        }
+    }
 });
